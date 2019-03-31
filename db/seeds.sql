293d55a4d78e53d78a969ab080f208e76bfbdf08
MaryB <marybmichaels@outlook.com>
2019-03-30 21:04:27 -0500

    mysql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, createdAt) VALUES ('Single Patty with Swiss Cheese', now());
+INSERT INTO burgers (burger_name, createdAt) VALUES ('Double Burger all the way', now());
+INSERT INTO burgers (burger_name, createdAt) VALUES ('Plain Burger with Fries', now());
+
+SELECT * FROM burgers;
